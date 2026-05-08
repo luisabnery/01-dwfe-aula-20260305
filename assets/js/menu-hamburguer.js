911a5bb5ece140ab2fc779137493043b0254de1d
Luísa Braga Nery de Lima <1554600@sga.pucminas.br>
2026-05-07 22:02:47 -0300

    Aleração aula 07 maio

---
+++ assets/js/menu-hamburguer.js
@@ -0,0 +1,4 @@
+function alternarVisualizacaoDoMenuLateral(){
+    const menu = document.querySelector("nav");
+    menu.classList.toggle("menu-oculto"); // toggle: chavear (se nao tem, coloqeu, se tem, retire! (alternar entre))
+}
